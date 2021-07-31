aacc98a4d82286c2e147242baf6da709f53ad45a
Julijana Ugrinov <jugrinov@gmail.com>
2021-07-31 23:26:56 +0200

    All repositories done

---
+++ GreenerGarden.Repositories/ExpenceRepository.cs
@@ -0,0 +1,60 @@
+﻿using GreenerGarden.Data;
+using GreenerGarden.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenerGarden.Repositories
+{
+    public interface IExpenceRepository : IRepository<Expence>
+    {
+
+    }
+
+    public class ExpenceRepository : IExpenceRepository
+    {
+        private readonly ApplicationDbContext _appContext;
+        public ExpenceRepository(ApplicationDbContext appContext)
+        {
+            _appContext = appContext;
+        }
+        public Expence GetById(int id)
+        {
+            var expence = _appContext.Expences.Find(id);
+            return expence;
+        }
+
+        public Expence Delete(int id)
+        {
+            Expence existing = _appContext.Expences.Find(id);
+            var result = _appContext.Expences.Remove(existing).Entity;
+            return result;
+        }
+
+        public async Task<IEnumerable<Expence>> GetAllAsync()
+        {
+            var data = await _appContext.Expences.ToListAsync();
+            return data;
+        }
+
+        public Expence Insert(Expence obj)
+        {
+            var data = _appContext.Expences.Add(obj).Entity;
+            return data;
+        }
+
+        public void Save()
+        {
+            _appContext.SaveChanges();
+        }
+
+        public Expence Update(Expence obj)
+        {
+            var updatedEntry = _appContext.Expences.Attach(obj).Entity;
+            _appContext.Entry(obj).State = EntityState.Modified;
+            return updatedEntry;
+        }
+    }
+}
---
+++ GreenerGarden.Repositories/NoteRepository.cs
@@ -0,0 +1,59 @@
+﻿using GreenerGarden.Data;
+using GreenerGarden.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenerGarden.Repositories
+{
+    public interface INoteRepository : IRepository<Note>
+    {
+
+    }
+    public class NoteRepository : INoteRepository
+    {
+        private readonly ApplicationDbContext _appContext;
+        public NoteRepository(ApplicationDbContext appContext)
+        {
+            _appContext = appContext;
+        }
+        public Note GetById(int id)
+        {
+            var note = _appContext.Notes.Find(id);
+            return note;
+        }
+
+        public Note Delete(int id)
+        {
+            Note existing = _appContext.Notes.Find(id);
+            var result = _appContext.Notes.Remove(existing).Entity;
+            return result;
+        }
+
+        public async Task<IEnumerable<Note>> GetAllAsync()
+        {
+            var data = await _appContext.Notes.ToListAsync();
+            return data;
+        }
+
+        public Note Insert(Note obj)
+        {
+            var data = _appContext.Notes.Add(obj).Entity;
+            return data;
+        }
+
+        public void Save()
+        {
+            _appContext.SaveChanges();
+        }
+
+        public Note Update(Note obj)
+        {
+            var updatedEntry = _appContext.Notes.Attach(obj).Entity;
+            _appContext.Entry(obj).State = EntityState.Modified;
+            return updatedEntry;
+        }
+    }
+}
