1be31daf1086ede289ec59ca4415c4fca9b6bdc3
Julijana Ugrinov <jugrinov@gmail.com>
2021-08-01 21:42:24 +0200

    New migration after entity name changed (yield to cropyield)

---
+++ GreenerGarden.Data/Migrations/20210801193929_ChangedYieldName.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GreenerGarden.Data.Migrations
+{
+    public partial class ChangedYieldName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Yields");
+
+            migrationBuilder.CreateTable(
+                name: "CropYields",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CultureId = table.Column<int>(nullable: false),
+                    Amount = table.Column<float>(nullable: false),
+                    SeasonId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CropYields", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CropYields_Cultures_CultureId",
+                        column: x => x.CultureId,
+                        principalTable: "Cultures",
+                        principalColumn: "CultureId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CropYields_Seasons_SeasonId",
+                        column: x => x.SeasonId,
+                        principalTable: "Seasons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CropYields_CultureId",
+                table: "CropYields",
+                column: "CultureId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CropYields_SeasonId",
+                table: "CropYields",
+                column: "SeasonId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CropYields");
+
+            migrationBuilder.CreateTable(
+                name: "Yields",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Amount = table.Column<float>(type: "real", nullable: false),
+                    CultureId = table.Column<int>(type: "int", nullable: false),
+                    SeasonId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Yields", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Yields_Cultures_CultureId",
+                        column: x => x.CultureId,
+                        principalTable: "Cultures",
+                        principalColumn: "CultureId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Yields_Seasons_SeasonId",
+                        column: x => x.SeasonId,
+                        principalTable: "Seasons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Yields_CultureId",
+                table: "Yields",
+                column: "CultureId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Yields_SeasonId",
+                table: "Yields",
+                column: "SeasonId");
+        }
+    }
+}
